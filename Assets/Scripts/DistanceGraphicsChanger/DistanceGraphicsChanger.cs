dfb5dc66120a31d0a280454d38e4b7e6c38ef041
Kim Ordén <kimmurgraal@gmail.com>
2021-01-30 08:58:57 +0200

    Add distanced graphics changer
    -Changes player graphics based on distance to other player

---
+++ Assets/Scripts/DistanceGraphicsChanger/DistanceGraphicsChanger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceGraphicsChanger : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer rend;
+    [SerializeField] private Transform toFind;
+    [SerializeField] private DistancedSpriteContainer sprites;
+    [SerializeField] private float intervalBetweenDistanceCheck;
+
+    private float startDistance;
+    private float timer;
+
+    private void Start()
+    {
+        startDistance = Vector2.Distance(toFind.position, transform.position);
+        timer = intervalBetweenDistanceCheck;
+    }
+
+    private void Update()
+    {
+        UpdateSpriteBasedOnDistanceOnIntervals();
+    }
+
+    public void UpdateSpriteBasedOnDistanceOnIntervals()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            var newSprite = CheckDistanceAndReturnNewSpriteOrCurrent();
+            if (newSprite != rend.sprite)
+            {
+                rend.sprite = newSprite;
+            }
+            timer = intervalBetweenDistanceCheck;
+        }
+    }
+
+    public Sprite CheckDistanceAndReturnNewSpriteOrCurrent()
+    {
+       if (sprites == null) return rend.sprite;
+       if (sprites.Values.Length == 0) return rend.sprite;
+
+       var currentDistance = Vector2.Distance(toFind.position, transform.position);
+       var percentageDistanceSinceStart = currentDistance / startDistance * 100;
+       foreach(var dSprite in sprites.Values)
+       {
+            Debug.Log(percentageDistanceSinceStart);
+            if (percentageDistanceSinceStart.IsInBetween(dSprite.minDistance,dSprite.maxDistance))
+            {
+                if(dSprite.sprite != null)
+                    return dSprite.sprite;
+            }
+       }
+
+       return rend.sprite;
+    }
+}
---
+++ Assets/Scripts/DistanceGraphicsChanger/DistancedSprite.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct DistancedSprite
+{
+    public Sprite sprite;
+    public float minDistance;
+    public float maxDistance; 
+}
