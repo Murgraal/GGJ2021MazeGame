c9bf351c49ec999a8cc90d5a14532a6ef8eefedb
Kim Ordén <kimmurgraal@gmail.com>
2021-01-30 10:52:53 +0200

    Add auto initialization to DistancedSpriteContainer.cs

--- Assets/Scripts/DistanceGraphicsChanger/DistancedSpriteContainer.cs
+++ Assets/Scripts/DistanceGraphicsChanger/DistancedSpriteContainer.cs
@@ -3,5 +3,19 @@
 [CreateAssetMenu(fileName = "DistancedSpriteContainer")]
 public class DistancedSpriteContainer : ScriptableObject
 {
-    public DistancedSprite[] Values; 
+    public DistancedSprite[] Values;
+    public void InitiliazeMinMaxValues()
+    {
+        var percentageFragment = 100 / Values.Length;
+        var reverseIterator = Values.Length -1;
+
+        for(int i = 0; i < Values.Length; i++)
+        {
+            Values[reverseIterator].minDistance = i * percentageFragment;
+            Values[reverseIterator].maxDistance = (i + 1) * percentageFragment;
+            reverseIterator--;
+        }
+
+        Values[0].maxDistance = 101;
+    }
 }
